29ce9b238631b63263b61d83756478b24def9eb7
Németi Dávid <david.nemeti@gmail.com>
2019-09-19 18:20:23 +0200

    nuget creation fixes and improvements

--- release.bat
+++ release.bat
@@ -1,2 +1,2 @@
 msbuild /m /p:Configuration=Release Castle.Windsor.Lifestyles.sln
-.nuget\nuget.exe pack Castle.Windsor.Lifestyles.nuspec
+.nuget\nuget.exe pack Castle.Windsor.Lifestyles\Castle.Windsor.Lifestyles.csproj -Properties Configuration=Release
